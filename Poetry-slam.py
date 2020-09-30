b14bb8c920ba778474f2791c1ccc5aef04ed5b88
Nolan <60010605+nolngo@users.noreply.github.com>
2020-09-30 15:22:54 -0700

    Update Poetry-slam.py

--- Poetry-slam.py
+++ Poetry-slam.py
@@ -2,7 +2,6 @@ filename = "poem.txt"
 def get_file_lines(filename):
     read_poem = open(filename, 'r')
     return read_poem.readlines()        #this function has outputted a list version of the text file! We can now reference it in following functions
-
 #print(get_file_lines(filename))     #this is to test the return output of the get_file_lines
 
 print("\u0332".join("THIS IS THE POEM BACKWARDS"))      #used the unicode \u0332 to underline the following text, the following text is the intro to the function.
@@ -26,12 +25,13 @@ lines_printed_random(get_file_lines(filename))
 
 print("\u0332".join("THIS IS THE POEM IN CUSTOM ORDER"))        #used the unicode \u0332 to underline the following text, the following text is the intro to the function.
 def lines_printed_custom(lines_list):
-    print("These lines begin with B or T")      #this for loop will identify and print only list items starting with B, T, or N
+    print("These lines begin with B or T or N")      #this for loop will identify and print only list items starting with B, T, or N
     for line in lines_list:
         if line[0] == "B" or line[0] == "T" or line[0] == "N":
             print(line)
-    print("These lines do not begin with B")       #this for look is much like an else function except i wanted to put a space in between them and put the message
+    print("These lines do not begin with B or T or N")       #this for look is much like an else function except i wanted to put a space in between them and put the message
     for line in lines_list:
         if line[0] != "B" or line[0] != "T" or line[0] != "N":
             print(line)
 lines_printed_custom(get_file_lines(filename))
+
