1405a0c071bf4327ef67e15b124fbec5b7c1c93f
Nolan <60010605+nolngo@users.noreply.github.com>
2020-09-30 14:16:56 -0700

    more comments

--- Poetry-slam.py
+++ Poetry-slam.py
@@ -7,7 +7,7 @@ def get_file_lines(filename):
 
 print("\u0332".join("THIS IS THE POEM BACKWARDS"))      #used the unicode \u0332 to underline the following text, the following text is the intro to the function.
 def lines_printed_backwards(lines_list):
-    lines_list = lines_list[::-1]
+    lines_list = lines_list[::-1]       #modified line_list to read backwards using a slice
     for line in lines_list:         #putting the print into a for loop allows the list to be printed in a nicer format, line by line
         print(line)
 
@@ -15,7 +15,7 @@ lines_printed_backwards(get_file_lines(filename))       #this is to call the bac
 
 
 print("\u0332".join("THIS IS THE POEM IN RANDOM ORDER"))        #used the unicode \u0332 to underline the following text, the following text is the intro to the function.
-import random
+import random                       #imports the random libraries from python and allows random functions in our program
 def lines_printed_random(lines_list):
     random.shuffle(lines_list)      #random.shuffle preserves the amount of lines in the original list and does not introduce repeated lines
     for line in lines_list:         #putting the print into a for loop allows the list to be printed in a nicer format, line by line
